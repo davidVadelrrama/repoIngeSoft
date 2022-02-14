9c842f6547807c81d422f46bbdd77b9b902017a8
Juan Medina <juansemedina30@gmail.com>
2022-02-14 12:33:32 -0500

    Dev Branch

--- UnAlgoritmoBienTrabajado.java
+++ UnAlgoritmoBienTrabajado.java
@@ -1,7 +1,22 @@
 import java.util.Scanner;
 public class Main{
+    public static int menu(Scanner reader){
+        int opt=0;
+        System.out.println("Que deseas hacer?"+
+            "\n1) Imprimir hello world"+
+            "\2) Imprimir bye world");
+
+    }
+
     public static void main (String[]args){
         Scanner reader=new Scanner (System.in);
-        System.out.println("hello World!");
+        int opt1=0;
+        opt1=menu();
+        switch (opt1){
+            case 1: System.out.println("hello World!");
+            break;
+            case 2: System.out.println("Bye World!");
+            break;
+        }
     }
 }
