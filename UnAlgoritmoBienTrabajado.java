d77cc9da533045c81800a8f2864c30888345f53d
Juan Medina <juansemedina30@gmail.com>
2022-02-14 12:37:52 -0500

    Si

--- UnAlgoritmoBienTrabajado.java
+++ UnAlgoritmoBienTrabajado.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
-public class Main{
+public class UnAlgoritmoBienTrabajado{
     public static int menu(Scanner reader){
         int opt=0;
         System.out.println("Que deseas hacer?"+
             "\n1) Imprimir hello world"+
             "\2) Imprimir bye world");
-
+        return opt;
     }
 
     public static void main (String[]args){
         Scanner reader=new Scanner (System.in);
         int opt1=0;
-        opt1=menu();
+        opt1=menu(reader);
         switch (opt1){
             case 1: System.out.println("hello World!");
             break;
